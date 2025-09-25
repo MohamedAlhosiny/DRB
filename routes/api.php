64222370f764424fd3ed84ee5b25dee7b7596eb2
MohamedAlhosiny <mohamedalsiri110@gmail.com>
2025-09-25 22:16:07 +0300

    CRUD operations With API

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\ProductController;
+use App\Http\Controllers\api\ProductController;
+use App\Models\Product;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Route::get('/product' , [ProductController::class , "index"])->name('product.index');
+
+Route::post('/product' , [ProductController::class , 'store']);
+Route::get('/product' , [ProductController::class , 'index']);
+Route::get('/product/{id}' , [ProductController::class , 'show']);
+Route::post('/product/{id}' , [ProductController::class , 'update']);
+Route::delete('/product/{id}' , [ProductController::class , 'destroy']);
